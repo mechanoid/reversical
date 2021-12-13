44ccf4fbb6c3a6f6d2e7981b1df9b77eb8cd4012
Falk Hoppe <falkhoppe81@gmail.com>
2021-12-13 13:07:36 +0100

    add initial version of namename

---
+++ README.md
@@ -0,0 +1,77 @@
+# namename
+
+a minimal named router for express.
+
+## Installation
+
+```
+npm install namename
+```
+
+## Usage
+
+Wrap your app in a NamedRouter to register new named routes:
+
+```
+import express from 'express'
+import { NamedRouter, routes } from 'namename'
+
+const app = express()
+const namedRouter = new NamedRouter(app)
+
+// register a named route
+namedRouter.get('people', '/people', (req, res) => { ... })
+namedRouter.post('people', '/people', (req, res) => { ... }) # second registration checks, that the same path is registered
+
+// route chaining also works
+namedRouter
+  .route('person', '/people/:id')
+  .get((req, res) => { ....})
+  .put((req, res) => { ....})
+
+
+// use `routes` for reverse routing.
+// A route is generate for each provided name with arguments (params = {}, query = {})
+routes.people({}, { search: 'search-term' }) // => /people?search=search-term
+routes.person({ id: 12 }) // => /people/12
+```
+
+### provide routes object to your view-Stack
+
+```
+app.locals = routes
+```
+
+### Sub Apps
+
+When mounting a sub-app to an existing express app, we receive the mountpath in the handler.
+This allows for named routes and reverse routing in sub apps as well.
+
+NOTE: Please have in mind that a `express.Router()` instance does not have access to the mountpath,
+as it is just a router in the same app instance. So please create sub-apps like shown below.
+
+sub-app.js
+```
+import express from 'express'
+const subApp = express()
+
+const namedRouter = new NamedRouter(subApp)
+
+namedRouter.route('dashboard', '/dashboard')
+  .get((req,res) => { ... })
+
+export default subApp
+```
+
+app.js
+```
+import express from 'express'
+import subApp from './sub-app.js'
+const app = express()
+
+const namedRouter = new NamedRouter(app)
+
+namedRouter.use('/admin', subApp)
+
+routes.dashboard() // => /admin/dashboard
+```
---
+++ index.js
@@ -0,0 +1,78 @@
+/* global encodeURIComponent, URL, URLSearchParams */
+import { format } from 'url'
+import { join } from 'path'
+import { compile } from 'path-to-regexp'
+
+const hasOwn = Object.prototype.hasOwn ? Object.prototype.hasOwn : Object.prototype.hasOwnProperty
+
+const supportedExpressMethods = [
+  'checkout', 'copy', 'delete', 'get', 'head', 'lock', 'merge', 'mkactivity', 'mkcol', 'move', 'm-search', 'notify', 'options', 'patch', 'post', 'purge', 'put', 'report', 'search', 'subscribe', 'trace', 'unlock', 'unsubscribe'
+]
+
+const pathnameToRelativeUrl = (pathname, query = {}) => {
+  const fakeHost = 'http://fakehost'
+  const resultUrl = new URL(pathname, fakeHost)
+  resultUrl.search = new URLSearchParams(query)
+  return format(resultUrl, { auth: false, fragment: false }).slice(fakeHost.length)
+}
+
+class Route {
+  constructor (path, { app } = {}) {
+    this.path = path
+    this.app = app
+  }
+
+  get routePath () {
+    return this.app.mountpath ? join(this.app.mountpath, this.path) : this.path
+  }
+
+  render (params = {}, query = {}) {
+    const toPath = compile(this.routePath, { encode: encodeURIComponent })
+    const pathname = toPath(params)
+    return pathnameToRelativeUrl(pathname, query)
+  }
+}
+
+class RouteRegistry {
+  constructor () {
+    this.routes = {}
+  }
+
+  register (name, path, { app } = {}) {
+    const route = new Route(path, { app })
+
+    if (!this.routes[name] && !hasOwn.call(this, name)) {
+      this.routes[name] = route
+      this[name] = this.routes[name].render.bind(this.routes[name])
+    } else if (this.routes[name].path !== path) {
+      throw new Error(`NamedRouting: registered resource for "${name}" is already registered with different path (${this.routes[name].path} conflicts with ${path}).`)
+    }
+    // route is already registered, do nothing
+  }
+}
+
+// initialize on first import
+export const routes = new RouteRegistry()
+
+export class NamedRouter {
+  constructor (app) {
+    this.app = app
+    this.mountpath = app.mountpath
+  }
+
+  route (name, path) {
+    routes.register(name, path, { app: this.app })
+    return this.app.route(path)
+  }
+
+  use (...args) {
+    return this.app.use(...args)
+  }
+}
+
+for (const method of supportedExpressMethods) {
+  NamedRouter.prototype[method] = function (name, path, handler) {
+    routes.register(name, path, { app: this.app })
+    return this.app[method](path, handler)
+  }
+}
