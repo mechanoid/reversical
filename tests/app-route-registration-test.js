7842220a04b15da9e67318ef2f6d9e4932b47752
Falk Hoppe <falkhoppe81@gmail.com>
2021-12-13 13:36:52 +0100

    add more tests for standard route registration

--- tests/app-route-registration-test.js
+++ tests/app-route-registration-test.js
@@ -12,4 +12,38 @@ test('create named route via app.get', () => {
   assert.equal(routes.simpleGetBoundRoute(), '/simple-route')
 })
 
+test('registration for the same name should be ok, when bound path is equal to already registered', () => {
+  const app = express()
+  const namedRouter = new NamedRouter(app)
+
+  namedRouter.get('simple-get-bound-route', '/simple-route', (req, res) => {})
+  assert.doesNotThrow(() => {
+    namedRouter.post('simple-get-bound-route', '/simple-route', (req, res) => {})
+  })
+
+  assert.equal(routes.simpleGetBoundRoute(), '/simple-route')
+})
+
+test('registration for the same name should fail, when bound path is not equal to already registered', () => {
+  const app = express()
+  const namedRouter = new NamedRouter(app)
+
+  namedRouter.get('simple-get-bound-route', '/simple-route', (req, res) => {})
+
+  assert.throws(() => {
+    namedRouter.post('simple-get-bound-route', '/different-route', (req, res) => {})
+  })
+})
+
+test('register multiple handlers via route chaining', () => {
+  const app = express()
+  const namedRouter = new NamedRouter(app)
+
+  namedRouter.route('chained-route', '/chained-routes')
+    .get((req, res) => {})
+    .post((req, res) => {})
+
+  assert.equal(routes.chainedRoute(), '/chained-routes')
+})
+
 test.run()
